d1c41670881a3287ef41a4ea9f2189ac5280c669
petersson.krill@gmail.com <petersson.krill@gmail.com@97dc9d3f-0307-c889-021d-4eb5648fda10>
2011-03-29 13:19:17 +0000

    Updated to version 0.2.0

--- default.py
+++ default.py
@@ -1,35 +1,19 @@
-import urllib,urllib2,re,xbmcplugin,xbmcgui,sc2casts,sys,os,xbmc
+import sys, xbmc, xbmcplugin, xbmcaddon, sc2casts
 
-Day = sc2casts.SC2Casts()
+__version__ = "0.1.0"
+__plugin__ = "SC2Casts-" + __version__
+__author__ = "Kristoffer Petersson"
+__settings__ = xbmcaddon.Addon(id='plugin.video.sc2casts')
+__language__ = __settings__.getLocalizedString
 
-params=Day.get_params()
-url=None
-mode=None
-Name=None
-duration=None
+SC2Casts = sc2casts.SC2Casts()
 
-try:
-        url=urllib.unquote_plus(params["url"])
-except:
-        pass
-try:
-        name=urllib.unquote_plus(params["name"])
-except:
-        pass
-try:
-        duration=urllib.unquote_plus(params["duration"])
-except:
-        pass
-try:
-        mode=int(params["mode"])
-except:
-        pass
-
-if mode==None or url==None or len(url)<1:
-    Day.categories() 
-if mode==2:
-    Day.index(url)  
-if mode==3:
-    Day.videolinks(url) 	
+if (not sys.argv[2]):
+    SC2Casts.root()
+else:
+	params = SC2Casts.getParams(sys.argv[2])
+	get = params.get
+	if (get("action")):
+		SC2Casts.action(params)
 	
 xbmcplugin.endOfDirectory(int(sys.argv[1]))
--- sc2casts.py
+++ sc2casts.py
@@ -1,57 +1,144 @@
-import urllib,urllib2,re,xbmcplugin,xbmcgui,sys,os,xbmc
+import urllib,urllib2,re,sys,os,string,xbmcaddon,xbmcgui,xbmcplugin
 		
 ###################################
 ########  Class SC2Casts  #########
 ###################################
 				
 class SC2Casts:	
+
+	USERAGENT = "Mozilla/5.0 (Windows; U; Windows NT 6.1; en-GB; rv:1.9.2.8) Gecko/20100722 Firefox/3.6.8"
+	__settings__ = xbmcaddon.Addon(id='plugin.video.sc2casts')
+	
+	# ------------------------------------- Main functions ------------------------------------- #
+	
+	def root(self):
+		self.addCategory('recent casts', 'http://www.sc2casts.com', 'rootRecent')
+		self.addCategory('top casts', 'http://www.sc2casts.com/top', 'rootTop')
+		self.addCategory('browse casts', 'http://www.sc2casts.com/browse', 'rootBrowse')
 		
-	def categories(self):
-		self.addDir('recent casts','http://www.sc2casts.com',2,'','')
+	def rootTop(self):
+		self.addCategory('top all time', 'http://www.sc2casts.com/top?all', 'topAll')
+		self.addCategory('top month', 'http://www.sc2casts.com/top?month', 'topMonth')
+		self.addCategory('top week', 'http://sc2casts.com/top?week', 'topWeek')
+		self.addCategory('top 24 hours', 'http://sc2casts.com/top', 'top24h')
 		
-	def addLink(self,name,url,iconimage,duration):
-		ok=True
-		liz=xbmcgui.ListItem(name, iconImage="DefaultVideo.png", thumbnailImage=iconimage)
-		liz.setInfo( type="Video", infoLabels={ "Title": name , "Duration": duration } )
+	def rootBrowse(self):
+		self.addCategory('browse matchups', 'http://www.sc2casts.com', 'browseMatchups')
+		self.addCategory('browse players', 'http://www.sc2casts.com/top', 'browsePlayers')
+		self.addCategory('browse casters', 'http://www.sc2casts.com/browse', 'browseCasters')
+
+	def addCategory(self,title,url,action):
+		url=sys.argv[0]+"?url="+urllib.quote_plus(url)+"&title="+urllib.quote_plus(title)+"&action="+urllib.quote_plus(action)
+		listitem=xbmcgui.ListItem(title, iconImage="DefaultFolder.png", thumbnailImage="DefaultFolder.png")
+		listitem.setInfo( type="Video", infoLabels={ "Title": title } )
+		xbmcplugin.addDirectoryItem(handle=int(sys.argv[1]), url=url, listitem=listitem, isFolder=True)
+		
+	def addDirectory(self,title,url,action):
+		url=sys.argv[0]+"?url="+urllib.quote_plus(url)+"&title="+urllib.quote_plus(title)+"&action="+urllib.quote_plus(action)
+		liz=xbmcgui.ListItem(title, iconImage="DefaultFolder.png", thumbnailImage="DefaultFolder.png")
+		liz.setInfo( type="Video", infoLabels={ "Title": title } )
+		xbmcplugin.addDirectoryItem(handle=int(sys.argv[1]),url=url,listitem=liz,isFolder=True)
+		
+	def addVideo(self,title,url):
+		# Check if URL is a 'fillUp' URL
+		if url != 'fillUp':
+			url = self.getVideoUrl(url)		
+		liz=xbmcgui.ListItem(title, iconImage="DefaultVideo.png", thumbnailImage="DefaultVideo.png")
+		liz.setInfo( type="Video", infoLabels={ "Title": title } )
 		xbmcplugin.addDirectoryItem(handle=int(sys.argv[1]),url=url,listitem=liz)
+
+	def action(self, params):
+		get = params.get
+		if (get("action") == "rootTop"):
+			self.rootTop()
+		if (get("action") == "rootBrowse"):
+			self.rootBrowse()
+		if (get("action") == "rootRecent"):
+			self.showTitles(params)
+		if (get("action") == "topAll"):
+			self.showTitles(params)
+		if (get("action") == "topMonth"):
+			self.showTitles(params)
+		if (get("action") == "topWeek"):
+			self.showTitles(params)
+		if (get("action") == "top24h"):
+			self.showTitles(params)
+		if (get("action") == "showGames"):
+			self.showGames(params)
 		
-	def addDir(self,name,url,mode,iconimage,duration):
-		u=sys.argv[0]+"?url="+urllib.quote_plus(url)+"&mode="+str(mode)+"&name="+urllib.quote_plus(name)+"&duration="+urllib.quote_plus(duration)
-		ok=True
-		liz=xbmcgui.ListItem(name, iconImage="DefaultFolder.png", thumbnailImage=iconimage)
-		liz.setInfo( type="Video", infoLabels={ "Title": name } )
-		xbmcplugin.addDirectoryItem(handle=int(sys.argv[1]),url=u,listitem=liz,isFolder=True)
-		
-	def index(self,url):
-		req = urllib2.Request(url)
-		req.add_header('User-Agent', 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-GB; rv:1.9.0.3) Gecko/2008092417 Firefox/3.0.3')
+	def getParams(self, paramList):	
+		splitParams = paramList[paramList.find('?')+1:].split('&')
+		paramsFinal = {}
+		for value in splitParams:
+			splitParams = value.split('=')
+			type = splitParams[0]
+			content = splitParams[1]
+			if type == 'url':
+				content = urllib.unquote_plus(content)
+			paramsFinal[type] = content
+		return paramsFinal
+			
+			
+	# ------------------------------------- show functions ------------------------------------- #
+	
+	def showTitles(self, params = {}):
+		get = params.get
+		req = urllib2.Request(get("url"))
+		req.add_header('User-Agent', self.USERAGENT)
 		response = urllib2.urlopen(req)
 		link=response.read()
 		response.close()
-		info=re.compile('<h2><a href="(.+?)"><b >(.+?)</b> vs <b >(.+?)</b>').findall(link)
+		
+		if get("action") == 'topAll' or get("action") == 'topMonth' or get("action") == 'topWeek' or get("action") == 'top24h':
+			info=re.compile('<h3><a href="(.+?)"><b >(.+?)</b> vs <b >(.+?)</b>').findall(link)
+		else:
+			info=re.compile('<h2><a href="(.+?)"><b >(.+?)</b> vs <b >(.+?)</b>').findall(link)
 		caster=re.compile('<a href="/.+?"><span class="caster_name">(.+?)</span></a>').findall(link)
+		matchup=re.compile('<span style="color:#cccccc">(.*?)</span>').findall(link)
 		for i in range(len(info)):
-			self.addDir(info[i][1] + " vs " + info[i][2] + " casted by " + caster[i],info[i][0],3,'','')
+			self.addDirectory(matchup[i] +' ' + info[i][1] + " vs " + info[i][2] + " casted by " + caster[i],info[i][0],'showGames')
 			
-	def videolinks(self,url):
-		url = 'http://sc2casts.com/'+url	
+	def showGames(self, params = {}):
+		get = params.get
+		url = 'http://sc2casts.com/'+get("url")
 		req = urllib2.Request(url)
-		req.add_header('User-Agent', 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-GB; rv:1.9.0.3) Gecko/2008092417 Firefox/3.0.3')
+		req.add_header('User-Agent', self.USERAGENT)
 		response = urllib2.urlopen(req)
 		link=response.read()
 		response.close()
-		info=re.compile('<param name="movie" value="http://www.youtube.com/v/(.+?)?fs=1&amp;hl=en_US"></param>').findall(link)
+		matchCount = re.compile('<div id="g(.+?)"(.+?)</div></div>').findall(link)
 		
-		url = 'http://www.youtube.com/watch?v='+info[0]+'&safeSearch=none&hl=en_us'
+		if len(matchCount) > 0:
+			for i in range(len(matchCount)):
+				videoContent=re.compile('<param name="movie" value="http://www.youtube.com/v/(.+?)\?.+?"></param>').findall(link)
+				if len(videoContent) == 0:
+					break
+				else:
+					for n in range(len(videoContent)):
+						self.addVideo('Game '+ str(n+1), videoContent[n])						
+					fillUp = len(matchCount)-n
+					if fillUp > 1:
+						for k in range(fillUp-1):
+							self.addVideo('Game '+ str(n+k+2), 'fillUp')
+					break
+		else:
+			videoContent=re.compile('<param name="movie" value="http://www.youtube.com/v/(.+?)\?.+?"></param>').findall(link)
+			self.addVideo('Game 1', videoContent[0])
+			
+			
+	# ------------------------------------- Data functions ------------------------------------- #
+	
+	def getVideoUrl(self, url):
+		url = 'http://www.youtube.com/watch?v='+url+'&safeSearch=none&hl=en_us'
 		req = urllib2.Request(url)
 		req.add_header('User-Agent', 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-GB; rv:1.9.0.3) Gecko/2008092417 Firefox/3.0.3')
 		response = urllib2.urlopen(req)
 		link=response.read()
 		response.close()
 			
-		fmtSource = re.findall('"fmt_url_map": "([^"]+)"', link);
+		fmtSource = re.findall('"fmt_url_map": "([^"]+)"', link)
 		fmt_url_map = urllib.unquote_plus(fmtSource[0]).split('|')
-		links = {};
+		links = {}
 			
 		for fmt_url in fmt_url_map:
 			if (len(fmt_url) > 7):
@@ -72,24 +159,23 @@ class SC2Casts:
 					else :
 						quality = "5"
 					links[int(quality)] = final_url.replace('\/','/')
-					
-		test = links.items()
-		self.addLink('Test',test[0][1],'','')
 		
-	def get_params(self):
-			param=[]
-			paramstring=sys.argv[2]
-			if len(paramstring)>=2:
-					params=sys.argv[2]
-					cleanedparams=params.replace('?','')
-					if (params[len(params)-1]=='/'):
-							params=params[0:len(params)-2]
-					pairsofparams=cleanedparams.split('&')
-					param={}
-					for i in range(len(pairsofparams)):
-							splitparams={}
-							splitparams=pairsofparams[i].split('=')
-							if (len(splitparams))==2:
-									param[splitparams[0]]=splitparams[1]
-									
-			return param
+		hd_quality = int(self.__settings__.getSetting( "hd_videos" ))
+		get = links.get
+		
+		# Select SD quality, standard
+		if (get(35)):
+			url = get(35)
+		elif (get(34)):
+			url = get(34)
+		
+		# Select HD quality if wanted
+		if (hd_quality > 0): # <-- 720p
+			if (get(22)):
+				url = get(22)
+		if (hd_quality > 1): # <-- 1080p
+			if (get(37)):
+				url = get(37)
+				
+		return url
+		
