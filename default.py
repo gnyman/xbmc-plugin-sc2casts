1f5436787cbb2512c89917247e38fd87804540b8
petersson.krill@gmail.com <petersson.krill@gmail.com@97dc9d3f-0307-c889-021d-4eb5648fda10>
2011-04-02 08:56:43 +0000

    Updated to version 0.3.0

--- default.py
+++ default.py
@@ -1,6 +1,6 @@
 import sys, xbmc, xbmcplugin, xbmcaddon, sc2casts
 
-__version__ = "0.2.2"
+__version__ = "0.3.0"
 __plugin__ = "SC2Casts-" + __version__
 __author__ = "Kristoffer Petersson"
 __settings__ = xbmcaddon.Addon(id='plugin.video.sc2casts')
@@ -11,6 +11,8 @@ SC2Casts = sc2casts.SC2Casts()
 if (not sys.argv[2]):
     SC2Casts.root()
 else:
+	print __plugin__
+
 	params = SC2Casts.getParams(sys.argv[2])
 	get = params.get
 	if (get("action")):
--- sc2casts.py
+++ sc2casts.py
@@ -9,7 +9,22 @@ class SC2Casts:
 	USERAGENT = "Mozilla/5.0 (Windows; U; Windows NT 6.1; en-GB; rv:1.9.2.8) Gecko/20100722 Firefox/3.6.8"
 	__settings__ = xbmcaddon.Addon(id='plugin.video.sc2casts')
 	
-	# ------------------------------------- Main functions ------------------------------------- #
+	def action(self, params):
+		get = params.get
+		if (get("action") == "rootTop"):
+			self.rootTop()
+		if (get("action") == "rootBrowse"):
+			self.rootBrowse()
+		if (get("action") == "browseEvents"):
+			self.browseEvents()
+		if (get("action") == "browseCasters"):
+			self.browseCasters()
+		if (get("action") == "showTitles" or get("action") == "showTitlesTop"):
+			self.showTitles(params)
+		if (get("action") == "showGames"):
+			self.showGames(params)
+	
+	# ------------------------------------- Menu functions ------------------------------------- #
 	
 	# display the root menu
 	def root(self):
@@ -27,11 +42,22 @@ class SC2Casts:
 	# display the browse casts menu
 	def rootBrowse(self):
 		self.addCategory('browse events', '', 'browseEvents')
+		self.addCategory('browse casters', '', 'browseCasters')
 	
 	# display the browse events menu
 	def browseEvents(self):
 		self.addCategory('TSL 3', 'http://sc2casts.com/event227-TSL-3', 'showTitles')
+		self.addCategory('Dreamhack', 'http://sc2casts.com/event144-DreamHack', 'showTitles')
+		self.addCategory('SC2Casts.com Invitational', 'http://sc2casts.com/event65-SC2Casts.com-Invitational', 'showTitles')
+		
+	# display the browse casters menu
+	def browseCasters(self):
+		self.addCategory('Day[9] & djWHEAT', 'http://sc2casts.com/caster97-Day9-&-djWHEAT', 'showTitles')
 
+		
+	# ------------------------------------- Add functions ------------------------------------- #
+
+	
 	def addCategory(self,title,url,action):
 		url=sys.argv[0]+"?url="+urllib.quote_plus(url)+"&title="+urllib.quote_plus(title)+"&action="+urllib.quote_plus(action)
 		listitem=xbmcgui.ListItem(title, iconImage="DefaultFolder.png", thumbnailImage="DefaultFolder.png")
@@ -52,58 +78,58 @@ class SC2Casts:
 		liz.setInfo( type="Video", infoLabels={ "Title": title } )
 		xbmcplugin.addDirectoryItem(handle=int(sys.argv[1]),url=url,listitem=liz)
 
-	def action(self, params):
-		get = params.get
-		if (get("action") == "rootTop"):
-			self.rootTop()
-		if (get("action") == "rootBrowse"):
-			self.rootBrowse()
-		if (get("action") == "browseEvents"):
-			self.browseEvents()
-		if (get("action") == "showTitles" or get("action") == "showTitlesTop"):
-			self.showTitles(params)
-		if (get("action") == "showGames"):
-			self.showGames(params)
-		
-	def getParams(self, paramList):	
-		splitParams = paramList[paramList.find('?')+1:].split('&')
-		paramsFinal = {}
-		for value in splitParams:
-			splitParams = value.split('=')
-			type = splitParams[0]
-			content = splitParams[1]
-			if type == 'url':
-				content = urllib.unquote_plus(content)
-			paramsFinal[type] = content
-		return paramsFinal
-			
-			
-	# ------------------------------------- show functions ------------------------------------- #
+
+	# ------------------------------------- Show functions ------------------------------------- #
+
 	
 	def showTitles(self, params = {}):
 		get = params.get
-		req = urllib2.Request(get("url"))
-		req.add_header('User-Agent', self.USERAGENT)
-		response = urllib2.urlopen(req)
-		link=response.read()
-		response.close()
+		link = self.getRequest(get("url"))
+		
+		# Get settings
+		boolMatchup = self.__settings__.getSetting( "matchup" )
+		boolNr_games = self.__settings__.getSetting( "nr_games" )
+		boolEvent = self.__settings__.getSetting( "event" )
+		boolRound = self.__settings__.getSetting( "round" )
+		boolCaster = self.__settings__.getSetting( "caster" )
+		
+		# Get info to show
+		caster = re.compile('<a href="/.+?"><span class="caster_name">(.*?)</span></a>').findall(link)
+		matchup = re.compile('<span style="color:#cccccc">(.*?)</span>').findall(link)
+		round = re.compile('<span class="round_name">(.*?)</span>').findall(link)
+		checkSource = re.compile('<span class="source_name">(.*?)</span>').findall(link)
+		event = re.compile('<span class="event_name".*?>(.*?)</span>').findall(link)
+		
+		#Different source if URL is .../top
 		if get("action") == 'showTitlesTop':
-			info=re.compile('<h3><a href="(.+?)"><b >(.+?)</b> vs <b >(.+?)</b>&nbsp;(.*?)</a></h3>').findall(link)
+			title = re.compile('<h3><a href="(.+?)"><b >(.+?)</b> vs <b >(.+?)</b>&nbsp;\((.*?)\)</a></h3>').findall(link)
 		else:
-			info=re.compile('<h2><a href="(.+?)"><b >(.+?)</b> vs <b >(.+?)</b>(.*?)</a></h2>').findall(link)
-		caster=re.compile('<a href="/.+?"><span class="caster_name">(.+?)</span></a>').findall(link)
-		matchup=re.compile('<span style="color:#cccccc">(.*?)</span>').findall(link)
-		for i in range(len(info)):
-			self.addDirectory(info[i][3] + ' - ' + matchup[i] +' ' + info[i][1] + " vs " + info[i][2] + " casted by " + caster[i],info[i][0],'showGames')
+			title = re.compile('<h2><a href="(.+?)"><b >(.+?)</b> vs <b >(.+?)</b> \((.*?)\)</a>').findall(link)
+
+		for i in range(len(event)):
+			if checkSource[i] != '@ YouTube':
+				pass
+			else:
+				url = ''
+				if boolMatchup == 'true':
+					url += matchup[i] + ' | '
+
+				url += title[i][1] + ' vs ' + title[i][2] + ' | '
+					
+				if boolNr_games == 'true':
+					url += title[i][3] + ' | '
+				if boolEvent == 'true':
+					url += event[i] + ' | '
+				if boolRound == 'true':
+					url += round[i] + ' | '
+				if boolCaster == 'true':
+					url += 'cast by: ' + caster[i]
+				
+				self.addDirectory(url,title[i][0],'showGames')
 			
 	def showGames(self, params = {}):
 		get = params.get
-		url = 'http://sc2casts.com/'+get("url")
-		req = urllib2.Request(url)
-		req.add_header('User-Agent', self.USERAGENT)
-		response = urllib2.urlopen(req)
-		link=response.read()
-		response.close()
+		link = self.getRequest('http://sc2casts.com/'+get("url"))
 		matchCount = re.compile('<div id="g(.+?)"(.+?)</div></div>').findall(link)
 		
 		if len(matchCount) > 0:
@@ -126,15 +152,30 @@ class SC2Casts:
 			
 			
 	# ------------------------------------- Data functions ------------------------------------- #
+
 	
-	def getVideoUrl(self, url):
-		url = 'http://www.youtube.com/watch?v='+url+'&safeSearch=none&hl=en_us'
+	def getParams(self, paramList):	
+		splitParams = paramList[paramList.find('?')+1:].split('&')
+		paramsFinal = {}
+		for value in splitParams:
+			splitParams = value.split('=')
+			type = splitParams[0]
+			content = splitParams[1]
+			if type == 'url':
+				content = urllib.unquote_plus(content)
+			paramsFinal[type] = content
+		return paramsFinal
+		
+	def getRequest(self, url):
 		req = urllib2.Request(url)
-		req.add_header('User-Agent', 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-GB; rv:1.9.0.3) Gecko/2008092417 Firefox/3.0.3')
+		req.add_header('User-Agent', self.USERAGENT)
 		response = urllib2.urlopen(req)
 		link=response.read()
 		response.close()
-			
+		return link
+	
+	def getVideoUrl(self, url):
+		link = self.getRequest('http://www.youtube.com/watch?v='+url+'&safeSearch=none&hl=en_us')
 		fmtSource = re.findall('"fmt_url_map": "([^"]+)"', link)
 		fmt_url_map = urllib.unquote_plus(fmtSource[0]).split('|')
 		links = {}
